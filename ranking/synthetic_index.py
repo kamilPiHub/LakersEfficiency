83f4d39ab960a4546ec87a70bb00dc5db5e3944a
kamilPiHub <pizon792@gmail.com>
2025-04-13 13:13:10 +0200

    update ranking

--- ranking/synthetic_index.py
+++ ranking/synthetic_index.py
@@ -5,5 +5,5 @@ def compute_synthetic_index(df, weights=None):
     Calculates synthetic index using weighted sum method.
     """
     if weights is None:
-        weights = np.ones(df.shape[1]) / df.shape[1]  # equal weights
+        weights = np.ones(df.shape[1]) / df.shape[1]
     return df.dot(weights)
